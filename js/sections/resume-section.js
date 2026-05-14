f0bf2d3ec5e1c3c6c242383e83254244db48bee9
kritique <128252454+kritiquekapital@users.noreply.github.com>
2026-05-14 17:07:20 -0400

    Update resume-section.js

--- js/sections/resume-section.js
+++ js/sections/resume-section.js
@@ -67,7 +67,7 @@ export const resumeSection = {
         },
         {
           title: "north shore community college",
-          copy: "Business Administration, GPA 3.58 | Cum Laude",
+          copy: "Associate of Science, GPA 3.58 | Cum Laude",
           subTitle: "coursework",
           subCopy: "Financial & Managerial Accounting, Principles of Management, Digital Marketing, Business Law, Micro & Macroeconomics, Statistics, Integrated Media Design, Visual Design Theory, Digital Photography, Speech & Communication, Composition I & II, American Literature II, World Cinema, Philosophy, Global Conflicts & Human Behaviors, History of the Soviet Union & Russia: Post 1917, Sociology, Personal Psychology, Biology, Pre-Calculus, Intermediate Algebra, Introduction to Hazardous Materials, and Paralegal Studies."
         }
